b2634d956c64ac86b3d2ae2a0a58e6eed7d57acb
root <mahatma@eu.by>
2018-07-23 02:18:01 +0300

    optimize (& change syntax) xssevent

--- src/xssevent.c
+++ src/xssevent.c
@@ -1,11 +1,11 @@
 /*
- simple run "<cmdline> --xss <state>" on XScreenSaver events
+ simple run "<cmdline> <state>" on XScreenSaver events
  examples:
-  xssevents xkbd-onoff --xrdb
-  xssevents xkbd-onoff --xrdb --sudo1 /usr/sbin/ya-nrg light 20 cores 0 save --sudo2 /usr/sbin/ya-nrg restore
+  xssevents xkbd-onoff --xrdb --xss
+  xssevents xkbd-onoff --lock --sudo1 /usr/sbin/ya-nrg light 20 save --sudo2 /usr/sbin/ya-nrg restore --xss
+  xssevents xkbd-onoff --xrdb --sudo1 /usr/sbin/ya-nrg light 20 cores 0 auto --sudo2 /usr/sbin/ya-nrg restore --xss
 */
 
-#include <stdlib.h>
 #include <unistd.h>
 #include <wait.h>
 #include <X11/Xlib.h>
@@ -13,32 +13,29 @@
 
 int main(int argc, char **argv) {
 	XEvent ev;
-	char **msg;
-	char **a;
-	int xssevent, xsserr, xss, i;
+	char **msg, *m;
+	int xssevent, i;
 	Display *dpy = XOpenDisplay(NULL);
 
-	if (!dpy || !XScreenSaverQueryExtension(dpy, &xssevent, &xsserr)) return 1;
+	if (!dpy || !XScreenSaverQueryExtension(dpy, &xssevent, &i)) return 1;
+	xssevent+=ScreenSaverNotify;
 	XScreenSaverSelectInput(dpy, XDefaultRootWindow(dpy), ScreenSaverNotifyMask|ScreenSaverCycleMask);
-	for(i=1; argv[i]; i++);
-	a=malloc(sizeof(char*)*(i+1));
-	for(i=1; argv[i]; i++) a[i-1]=argv[i];
-	a[i-1]="--xss";
-	a[i+1]=NULL;
-	msg=&a[i];
+	for(i=1; i<argc; i++) argv[i-1]=argv[i];
+	msg = &argv[argc-1];
 	while (1) {
 		XNextEvent(dpy, &ev);
-		if (ev.type == xssevent+ScreenSaverNotify) {
+		if (ev.type == xssevent) {
 			switch (((XScreenSaverNotifyEvent*)&ev)->state) {
-			case ScreenSaverOff: *msg="off";break;
-			case ScreenSaverOn: *msg="on";break;
-			case ScreenSaverCycle: *msg="cycle";break;
-			case ScreenSaverDisabled: *msg="disabled";break;
-			default: *msg="unknown";
+			case ScreenSaverOff: m="off";break;
+			case ScreenSaverOn: m="on";break;
+			case ScreenSaverCycle: m="cycle";break;
+			case ScreenSaverDisabled: m="disabled";break;
+			default: m="unknown";
 			}
+			*msg=m;
 			while(waitpid(-1,NULL,WNOHANG)>0);
 			if (!fork()){
-				execvp(a[0], a);
+				execvp(argv[0], argv);
 				return 1;
 			}
 		}
