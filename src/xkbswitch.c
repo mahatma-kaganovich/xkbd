3b2d81387e1171013f6f3631ffa3e6220584388e
root <mahatma@eu.by>
2020-02-06 03:28:58 +0300

    add xkbswitch - simple next kb layout

---
+++ src/xkbswitch.c
@@ -0,0 +1,14 @@
+/* simple switch layout */
+
+#include <X11/Xlib.h>
+#include <X11/XKBlib.h>
+
+int main(){
+	Display *dpy = XOpenDisplay(NULL);
+	XkbStateRec s;
+
+	XkbGetState(dpy,XkbUseCoreKbd,&s);
+	XkbLockGroup(dpy,XkbUseCoreKbd,s.group+1);
+	XFlush(dpy);
+	return 0;
+}
