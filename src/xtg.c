d6e8dab804be2551b9ab1be3bfc2a1962ff13b6a
root <mahatma@eu.by>
2021-06-02 16:10:51 +0300

    nightly crazy bug

--- src/xtg.c
+++ src/xtg.c
@@ -1980,8 +1980,8 @@ static _short xiClasses(XIAnyClassInfo **classes, int num_classes){
 	if (xABS[2].en != xABS[0].en) xABS[2].en = 0;
 //	if ((type1&o_absolute) && xABS[0].en && !(xABS[0].max_min > 0.) && !(xABS[1].max_min > 0.))
 //		xABS[0].en = xABS[1].en = xABS[2].en = 0;
-	return type1;
 #endif
+	return type1;
 }
 
 static void getHierarchy(){
