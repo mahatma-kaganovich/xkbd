823425abd8371aa5d5c1ef520f393536cb0a0128
root <mahatma@eu.by>
2025-12-28 06:37:35 +0300

    add simple alloc

--- src/mk.sh
+++ src/mk.sh
@@ -37,7 +37,7 @@ _c(){
 }
 
 [ /usr/include/X11/keysymdef.h -nt ks2unicode.c ] && e ./ks2unicode.pl
-_c xkbd "box.c button.c kb.c ks2unicode.c xkbd.c" "xtst xi xrandr xft xpm" "-DVERSION=\"1.8.999\" -DDEFAULTCONFIG=\"/etc/xkbd.conf\" -DUSE_XFT -DUSE_XPM -DUSE_SS -DUSE_XI -DUSE_XR"
+_c xkbd "stalloc.c box.c button.c kb.c ks2unicode.c xkbd.c" "xtst xi xrandr xft xpm" "-DVERSION=\"1.8.999\" -DDEFAULTCONFIG=\"/etc/xkbd.conf\" -DUSE_XFT -DUSE_XPM -DUSE_SS -DUSE_XI -DUSE_XR"
 _c xssevent xssevent.c xscrnsaver
 _c xkbswitch xkbswitch.c
 _c xkbdd xtg.c "" -DNO_ALL
---
+++ src/stalloc.h
@@ -0,0 +1,1 @@
+void *stalloc(int l);
