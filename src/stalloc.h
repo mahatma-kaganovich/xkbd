27ab8481e0638120988f880fa8542226d1bce45d
root <mahatma@eu.by>
2026-01-04 02:36:43 +0300

    xkbd: stalloc customize align

--- src/stalloc.h
+++ src/stalloc.h
@@ -1,15 +1,24 @@
 
-// 128-bit alignment everywhere
+// 128-bit alignment if possible
 #define STALLOC 4
 
 void *stalloc(size_t l);
 
+#undef _ALIGN
 #ifndef STALLOC
 #define calloc1(s) calloc(1,sizeof(s))
 #define malloc1(s) malloc(sizeof(s))
 #define malloc2(s) malloc(s)
 #else
+
+#if _POSIX_C_SOURCE >= 200112L
 #define _ALIGN STALLOC
+#else
+// 64-bit malloc
+#define _ALIGN 3
+#error xxx
+#endif
+
 #define _align(s) ((((s+((1<<_ALIGN)-1)))>>_ALIGN)<<_ALIGN)
 #define calloc1(s) stalloc(_align(sizeof(s)))
 #define malloc1(s) stalloc(_align(sizeof(s)))
