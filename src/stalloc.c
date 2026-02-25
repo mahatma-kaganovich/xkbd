2b2b7cc1f3b6827b71d82b0547f6af5821fc8f8d
mahatma <mahatma@eu.by>
2026-02-25 15:56:20 +0300

    stalloc respect default aligning

--- src/stalloc.c
+++ src/stalloc.c
@@ -1,9 +1,13 @@
-//#define _GNU_SOURCE
 #include <stdlib.h>
 //#include <stdio.h>
 #include <string.h>
-
 #include "stalloc.h"
+#if __STDC_VERSION__ >= 201112L
+#include <stddef.h>
+#define CALIGN alignof(max_align_t)
+#else
+#define CALIGN 1
+#endif
 
 /* static alloc */
 
@@ -17,18 +21,16 @@ stalloc_buf;
 
 void *_calloc(size_t l){
 	void *p;
-#if _ALIGN && !(defined(MINIMAL) && _ALIGN < 4)
-	if
+	if ((CALIGN&((1<<_ALIGN)-1)) &&
 #if _POSIX_C_SOURCE >= 200112L
-	    (!posix_memalign(&p,_align(1),l)) memset(p,0,l);
+	    !posix_memalign(&p,_align(1),l)
 #elif __STDC_VERSION__ >= 201112L || defined(_ISOC11_SOURCE)
-	    ((p=aligned_alloc(_align(1),l))) memset(p,0,l);
+	    (p=aligned_alloc(_align(1),l))
 #else
 //#elif _XOPEN_SOURCE >= 500
-	    ((p=valloc(l))) memset(p,0,l);
+	    (p=valloc(l))
 #endif
-	else
-#endif
+	) memset(p,0,l); else
 	    {
 		//fprintf(stderr,"ERROR posix_memalign %i\n",_align(1));
 		p=calloc(1,l);
